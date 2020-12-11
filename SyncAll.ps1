f195bf135bd6ce9682042f357a2127d6dfe98ae0
Tom Cumbow <tcumbow@icloud.com>
2020-12-11 16:52:30 -0500

    folderized

---
+++ SyncAll.ps1
@@ -0,0 +1,5 @@
+unison .\VotansFisherman A:\VotansFisherman -auto -batch
+pause
+unison .\VotansFisherman "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\VotansFisherman" -auto -batch
+pause
+unison .\VotansFisherman R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\VotansFisherman -auto -batch
---
+++ SyncMe.ps1
@@ -0,0 +1,1 @@
+unison .\VotansFisherman A:\VotansFisherman\ -auto -batch
