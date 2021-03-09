3fb8fece95764350f7aaa11b40460cb67869a0d0
Tom Cumbow <tcumbow@icloud.com>
2021-03-08 19:18:56 -0500

    Version: 1.14.3

--- VotansFisherman/VotansFisherman100015.lua
+++ VotansFisherman/VotansFisherman100015.lua
@@ -2507,7 +2507,7 @@ function data:InitSettings()
 		name = data.title,
 		displayName = data.title,
 		author = "votan",
-		version = "1.14.2",
+		version = "1.14.3",
 		-- slashCommand = "",
 		-- registerForRefresh = true,
 		registerForDefaults = true,
