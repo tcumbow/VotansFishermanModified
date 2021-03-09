86c459f78a689179719a9536bb52a683c1515f35
Tom Cumbow <tcumbow@icloud.com>
2021-03-08 19:20:51 -0500

    Update Deploy.ps1

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,2 +1,2 @@
-robocopy .\VotansFisherman "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\VotansFisherman" /mir
-robocopy .\VotansFisherman R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\VotansFisherman /mir
+robocopy .\VotansFisherman "S:\VotansFisherman" /mir
+robocopy .\VotansFisherman J:\VotansFisherman /mir
